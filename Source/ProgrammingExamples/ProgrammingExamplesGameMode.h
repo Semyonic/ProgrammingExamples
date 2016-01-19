8fb2362bb1f96ab50a7750bcd5f17c40f6524da2
Semyonic <semih.onay@bilgiedu.net>
2016-01-20 00:46:48 +0200

    Initialize

---
+++ Source/ProgrammingExamples/ProgrammingExamplesGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ProgrammingExamplesGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROGRAMMINGEXAMPLES_API AProgrammingExamplesGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
