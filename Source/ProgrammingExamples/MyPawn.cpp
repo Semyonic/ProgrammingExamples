0b5a59f964313a455bb0ab42d5dd4dbf9c869069
Semyonic <semih.onay@bilgiedu.net>
2016-01-20 22:13:26 +0200

    Update

---
+++ Source/ProgrammingExamples/MyPawn.cpp
@@ -0,0 +1,42 @@
+//
+//  MyPawn.cpp
+//  ProgrammingExamples
+//
+//  Created by Semih Onay on 20.01.2016.
+//  Copyright © 2016 Semih Onay. All rights reserved.
+//
+
+#include "ProgrammingExamples.h"
+#include "MyPawn.h"
+
+
+// Sets default values
+AMyPawn::AMyPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+    AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+}
+
+// Called when the game starts or when spawned
+void AMyPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyPawn::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+// Called to bind functionality to input
+void AMyPawn::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+}
+
---
+++ Source/ProgrammingExamples/MyPawn.h
@@ -0,0 +1,34 @@
+//
+//  MyPawn.h
+//  ProgrammingExamples
+//
+//  Created by Semih Onay on 20.01.2016.
+//  Copyright © 2016 Semih Onay. All rights reserved.
+//
+
+#pragma once
+
+#include "GameFramework/Pawn.h"
+#include "MyPawn.generated.h"
+
+UCLASS()
+class PROGRAMMINGEXAMPLES_API AMyPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AMyPawn();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+
+	
+	
+};
